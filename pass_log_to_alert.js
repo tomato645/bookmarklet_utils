1f20dc97730803ca63c60863b03817637920f684
tomato645 <89794096+tomato645@users.noreply.github.com>
2024-01-03 21:12:12 +0900

    Initial commit

---
+++ pass_log_to_alert.js
@@ -0,0 +1,15 @@
+var isConsoleOverwritten;
+console.log(isConsoleOverwritten)
+if (!isConsoleOverwritten) {
+    window.console.log = function(message) {
+        alert(message);
+    }
+    window.console.warn = function(message) {
+        alert(message);
+    }
+    window.console.error = function(message) {
+        alert(message);
+    }
+}
+
+window.isConsoleOverwritten = true;
