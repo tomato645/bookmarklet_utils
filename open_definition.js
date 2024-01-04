ab27bca9b86a9790313451b4e633199177dfca06
tomato645 <89794096+tomato645@users.noreply.github.com>
2024-01-04 15:57:35 +0900

    Change initial process

--- open_definition.js
+++ open_definition.js
@@ -1,22 +1,18 @@
-(function(){
+(function() {
     var isOpenNewTabForSearchingEnable;
-    if (!isOpenNewTabForSearchingEnable){
+    if (!isOpenNewTabForSearchingEnable) {
         let button = document.createElement("button");
-        button.id = "open-new-tab-for-searching"
-        button.style = "position:fixed; right:50px; buttom:50px; z-index:100;";
-        
-        document.onselectionchange = function() {
-            let text = document.getSelection().toString();
-            console.log(text);
-            
-            button.innerHTML = `Open new tab about <strong>${text}</strong>`;
-            
-            button.onclick = function() {
-                console.log("Open new tab!!!!");
-                window.open(`https://duckduckgo.com/?q=${text}`, "_blank");
-            }
+        button.id = "open-new-tab-for-searching";
+        button.innerHTML = "Open new tab about ..."
+        button.style = `position:fixed; right:50px; bottom:50px; z-index:100; opacity:0.9;`;
+        document.body.prepend(button);
 
-            document.body.prepend(button);
-        }
-    }
+        document.onselectionchange = () => {
+            let text = document.getSelection().toString();
+            button.innerHTML = `Open new tab about <strong>${text}</strong>`;
+            button.onclick = () => {
+                window.open(`https://duckduckgo.com/?q=${text}`, "_blank");
+            };
+        };
+    };
 }())
