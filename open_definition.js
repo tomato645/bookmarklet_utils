84c51a669cfbdbbab77613e610e9da40dc9ed075
tomato645 <89794096+tomato645@users.noreply.github.com>
2024-01-04 16:38:41 +0900

    update

--- open_definition.js
+++ open_definition.js
@@ -1,18 +1,36 @@
 (function () {
-  var isOpenNewTabForSearchingEnable;
-  if (!isOpenNewTabForSearchingEnable) {
-    let button = document.createElement("button");
-    button.id = "open-new-tab-for-searching";
-    button.innerHTML = "Open new tab about ...";
-    button.style = `position:fixed; right:50px; bottom:50px; z-index:100; opacity:0.9;`;
-    document.body.prepend(button);
+    var isOpenNewTabForSearchingEnable;
+    if (!isOpenNewTabForSearchingEnable) {
+        let button = document.createElement("button");
+        button.id = "open-new-tab-for-searching";
+        button.style = `
+        position:fixed;
+        right:20px;
+        bottom:20px;
+        z-index:100;
+        opacity:0.9;
+        `;
+        document.body.prepend(button);
+        refresh();
 
-    document.onselectionchange = () => {
-      let text = document.getSelection().toString();
-      button.innerHTML = `Open new tab about <strong>${text}</strong>`;
-      button.onclick = () => {
-        window.open(`https://duckduckgo.com/?q=${text}`, "_blank");
-      };
-    };
-  }
+        function refresh() {
+            button.innerHTML = "Open new tab about ";
+            let text = document.getSelection().toString();
+            let spaceCount = (text.match(/ /g) || []).length;
+            let appendText = "";
+            if (spaceCount == 0) {
+                appendText = "<strong>...</strong>";
+            } else if (spaceCount > 5) {
+                appendText = `<strong>${text}</strong>`;
+            } else {
+                appendText = `<strong>${text}</strong>`;
+            }
+            button.innerHTML += appendText;
+            button.onclick = () => {
+                window.open(`https://duckduckgo.com/?q=${text}`, "_blank");
+            };
+        }
+
+        document.onselectionchange = refresh;
+    }
 })();
