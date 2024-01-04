b4d6e995a25184c0f2705d962d310e66f8abce93
tomato645 <89794096+tomato645@users.noreply.github.com>
2024-01-04 16:06:03 +0900

    Fix lack of semicolon

--- open_definition.js
+++ open_definition.js
@@ -1,18 +1,18 @@
-(function() {
-    var isOpenNewTabForSearchingEnable;
-    if (!isOpenNewTabForSearchingEnable) {
-        let button = document.createElement("button");
-        button.id = "open-new-tab-for-searching";
-        button.innerHTML = "Open new tab about ..."
-        button.style = `position:fixed; right:50px; bottom:50px; z-index:100; opacity:0.9;`;
-        document.body.prepend(button);
+(function () {
+  var isOpenNewTabForSearchingEnable;
+  if (!isOpenNewTabForSearchingEnable) {
+    let button = document.createElement("button");
+    button.id = "open-new-tab-for-searching";
+    button.innerHTML = "Open new tab about ...";
+    button.style = `position:fixed; right:50px; bottom:50px; z-index:100; opacity:0.9;`;
+    document.body.prepend(button);
 
-        document.onselectionchange = () => {
-            let text = document.getSelection().toString();
-            button.innerHTML = `Open new tab about <strong>${text}</strong>`;
-            button.onclick = () => {
-                window.open(`https://duckduckgo.com/?q=${text}`, "_blank");
-            };
-        };
+    document.onselectionchange = () => {
+      let text = document.getSelection().toString();
+      button.innerHTML = `Open new tab about <strong>${text}</strong>`;
+      button.onclick = () => {
+        window.open(`https://duckduckgo.com/?q=${text}`, "_blank");
+      };
     };
-}())
+  }
+})();
