f62e4f785cdc63d4ad95820657425736bc1cd3af
tomato645 <89794096+tomato645@users.noreply.github.com>
2024-01-04 15:58:08 +0900

    update

--- pass_log_to_alert.js
+++ pass_log_to_alert.js
@@ -1,15 +1,17 @@
-var isConsoleOverwritten;
-console.log(isConsoleOverwritten)
-if (!isConsoleOverwritten) {
-    window.console.log = function(message) {
-        alert(message);
-    }
-    window.console.warn = function(message) {
-        alert(message);
-    }
-    window.console.error = function(message) {
-        alert(message);
-    }
-}
+(function () {
+  var isConsoleOverwritten;
+  console.log(isConsoleOverwritten);
+  if (!isConsoleOverwritten) {
+    window.console.log = function (message) {
+      alert(message);
+    };
+    window.console.warn = function (message) {
+      alert(message);
+    };
+    window.console.error = function (message) {
+      alert(message);
+    };
+  }
 
-window.isConsoleOverwritten = true;
+  window.isConsoleOverwritten = true;
+})();
